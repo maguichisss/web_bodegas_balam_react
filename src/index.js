c54c89ec3682e97bfb5b0c9280129ed6af193e84
Mau <maguichisss@gmail.com>
2021-02-20 21:08:28 -0600

    Routing was added for NavigationBar adn pages

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//<React.StrictMode>
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
